208d8f65d6eef24476ba28bb9eebe9512b65ddfa
duducheng <jekyll168@icloud.com>
2021-01-08 17:51:06 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+import acsconv
+
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
+setup(
+    name='ACSConv',
+    version=acsconv.__version__,
+    url='https://github.com/M3DV/ACSConv',
+    license='Apache-2.0 License',
+    author='Jiancheng Yang and Xiaoyang Huang',
+    author_email='jekyll4168@sjtu.edu.cn',
+    description='[IEEE JBHI] Reinventing 2D Convolutions for 3D Images',
+    long_description=readme(),
+    packages=find_packages(),
+    zip_safe=True
+)
